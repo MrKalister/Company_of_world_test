27768955c411a71e87e8c4bbbe683961b6eeeda9
Novikov Maxim <maxon.nowik@yandex.ru>
2023-10-20 17:15:06 +0400

    Fix remarks.

--- api/utilities.py
+++ api/utilities.py
@@ -1,9 +1,7 @@
 import time
-from typing import Any, Dict, Union
 
 import requests
 
-from city.models import City
 from config.settings import PLUG, env
 
 API_KEY = env.str('YANDEX_API_KEY')
@@ -19,25 +17,21 @@ if PLUG:
 test_data = {'temp': 12, 'pressure_mm': 764, 'wind_speed': 2.8}
 
 
-def get_weather(city: City) -> Dict[str, Union[str, int]]:
+def get_weather(city):
     """Return the weather for a city from the weather service with caching."""
 
     # Check if weather data for this city is in the cache and not expired
     if city in weather_cache:
-        cached_data: Dict[str, Union[str, int]] = weather_cache[city][0]
-        timestamp: float = weather_cache[city][1]
-        current_time: float = time.time()
+        cached_data, timestamp = weather_cache[city]
+        current_time = time.time()
 
         # If the data is still fresh, return it
         if current_time - timestamp <= CACHE_EXPIRY:
             return cached_data
 
-    params: Dict[str, Union[str, float]] = {
-        'lat': city.latitude,
-        'lon': city.longitude,
-    }
+    params = {'lat': city.latitude, 'lon': city.longitude}
 
-    data: Dict[str, Union[str, int]] = (
+    data = (
         (
             requests.get(YANDEX_WEATHER_URL, params=params, headers=HEADERS)
             .json()
@@ -48,13 +42,17 @@ def get_weather(city: City) -> Dict[str, Union[str, int]]:
     )
 
     # Parse the data and store it in the cache with the current timestamp
-    weather_data: Dict[str, Union[str, int]] = parse_data(data)
-    weather_cache[city] = (weather_data, current_time)
+    weather_data = parse_data(data)
+    weather_cache[city] = (weather_data, time.time())
 
     return weather_data
 
 
-def parse_data(data: Dict[str, Any]) -> Dict[str, Union[str, int]]:
+def parse_data(data):
     """Parse the result from the weather service."""
-    keys_to_extract = ['temp', 'pressure_mm', 'wind_speed']
-    return {key: data.get(key) for key in keys_to_extract}
+
+    return {
+        'temp': data.get('temp'),
+        'pressure_mm': data.get('pressure_mm'),
+        'wind_speed': data.get('wind_speed'),
+    }
